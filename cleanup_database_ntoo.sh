a793926747bc01f9430266a72465901bb24a10c5
Rowland Mosbergen <mosbergen.r@wehi.edu.au>
2023-06-12 20:40:56 +1000

    making it ready for merging

--- cleanup_database_ntoo.sh
+++ cleanup_database_ntoo.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cp database_ntoo.tsv database_ntoo.tsv.bak
+
 cat database_ntoo.tsv | sed s/‚Äò/\'/g > test_clean_database_ntoo.tsv
 cat test_clean_database_ntoo.tsv | sed s/‚Äô/\'/g > test_clean_database_ntoo1.tsv
 cat test_clean_database_ntoo1.tsv | sed s/‚Äù/\"/g > test_clean_database_ntoo.tsv
@@ -8,3 +10,5 @@ cat test_clean_database_ntoo1.tsv | sed s/‚Äî/-/g > test_clean_database_ntoo
 
 
 diff database_ntoo.tsv test_clean_database_ntoo.tsv
+mv test_clean_database_ntoo.tsv database_ntoo.tsv
+rm test_clean_database_ntoo1.tsv
