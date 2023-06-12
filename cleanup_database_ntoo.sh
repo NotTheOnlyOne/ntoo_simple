40f88c5574269e6fcc7ed4e3786e29115dc49f3c
Rowland Mosbergen <mosbergen.r@wehi.edu.au>
2023-06-12 20:25:37 +1000

    using sed to clean out simple things

---
+++ cleanup_database_ntoo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cat database_ntoo.tsv | sed s/‚Äò/\'/g > test_clean_database_ntoo.tsv
+cat test_clean_database_ntoo.tsv | sed s/‚Äô/\'/g > test_clean_database_ntoo1.tsv
+cat test_clean_database_ntoo1.tsv | sed s/‚Äù/\"/g > test_clean_database_ntoo.tsv
+cat test_clean_database_ntoo.tsv | sed s/‚Äú/\"/g > test_clean_database_ntoo1.tsv
+
+
+diff database_ntoo.tsv test_clean_database_ntoo1.tsv
