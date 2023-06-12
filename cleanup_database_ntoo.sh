c4c97404a828a127569680f2bc94ca53470b4014
Rowland Mosbergen <mosbergen.r@wehi.edu.au>
2023-06-12 20:27:22 +1000

    Adding one for -

--- cleanup_database_ntoo.sh
+++ cleanup_database_ntoo.sh
@@ -4,7 +4,7 @@ cat database_ntoo.tsv | sed s/‚Äò/\'/g > test_clean_database_ntoo.tsv
 cat test_clean_database_ntoo.tsv | sed s/‚Äô/\'/g > test_clean_database_ntoo1.tsv
 cat test_clean_database_ntoo1.tsv | sed s/‚Äù/\"/g > test_clean_database_ntoo.tsv
 cat test_clean_database_ntoo.tsv | sed s/‚Äú/\"/g > test_clean_database_ntoo1.tsv
-cat test_clean_database_ntoo1.tsv | sed s/lll/\"/g > test_clean_database_ntoo.tsv
+cat test_clean_database_ntoo1.tsv | sed s/‚Äî/-/g > test_clean_database_ntoo.tsv
 
 
 diff database_ntoo.tsv test_clean_database_ntoo.tsv
