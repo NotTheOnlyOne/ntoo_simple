07c5553459bd5914b7446c73f3bfd40f08683e19
Rowland Mosbergen <mosbergen.r@wehi.edu.au>
2023-10-22 16:19:39 +1100

    Updating database_ntoo.tsv

--- app.py
+++ app.py
@@ -61,5 +61,5 @@ def index():
             return render_template('index.html',results=results,total_count=total_count)
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5001)
 
